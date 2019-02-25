53f82e635834589608f204fadf9eb3fd9c4a4338
johnpharmd <jhumphre@uw.edu>
2019-02-25 12:56:38 -0800

    Add new file that appears to be factored for password protection

---
+++ analyze_twitter_data.py
@@ -0,0 +1,9 @@
+#!/bin/usr/env python 3
+'''
+This module is the beginning of analytics in the cloud for johnpharmd
+'''
+from env_vars import check_password
+
+api_dict = check_password()
+print(api_dict)
+
